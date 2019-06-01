6636bc5beaea4a76f53cd70e5968d103346d34ff
Dominik <domii894@gmail.com>
2019-06-02 00:40:19 +0200

    Add iOS demo for QR code detection.

--- ios/Classes/SwiftQrScanPlugin.swift
+++ ios/Classes/SwiftQrScanPlugin.swift
@@ -9,6 +9,16 @@ public class SwiftQrScanPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    //result("iOS " + UIDevice.current.systemVersion)
+    let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: nil)
+    let image = CIImage(contentsOf: URL(string: "https://support.apple.com/library/content/dam/edam/applecare/images/de_DE/iOS/ios12-iphone-x-camera-open-safari-qr-code.jpg")!)
+    let features = detector?.features(in: image!)
+    features?.forEach({(feature) -> Void in
+        if let qrFeature = feature as? CIQRCodeFeature {
+                result(qrFeature.messageString)
+            }
+        }
+    )
+    //result("count: \(features?.count)")
   }
 }
