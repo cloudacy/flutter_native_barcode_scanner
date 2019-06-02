243eb2bb598b4c83ca1a3d00659cc087e0ad3661
Dominik <domii894@gmail.com>
2019-06-02 13:07:50 +0200

    Implement first steps for a camera preview in iOS.

--- ios/Classes/SwiftQrScanPlugin.swift
+++ ios/Classes/SwiftQrScanPlugin.swift
@@ -2,54 +2,83 @@ import Flutter
 import UIKit
 import AVFoundation
 
+public class SwiftQrScanPluginTexture: NSObject, FlutterTexture {
+  private(set) var latestPixelBuffer: CVPixelBuffer?
+  var onFrameAvailable: (() -> Void)?
+  
+  public func copyPixelBuffer() -> Unmanaged<CVPixelBuffer>? {
+    var pixelBuffer = latestPixelBuffer
+    var ptr: UnsafeMutableRawPointer? = UnsafeMutableRawPointer(&latestPixelBuffer)
+    let ptr2: UnsafeMutablePointer<UnsafeMutableRawPointer?> = UnsafeMutablePointer(&ptr)
+    while !OSAtomicCompareAndSwapPtrBarrier(&pixelBuffer, nil, ptr2) {
+      pixelBuffer = latestPixelBuffer
+    }
+    return Unmanaged<CVPixelBuffer>.passRetained(latestPixelBuffer!)
+  }
+}
+
 public class SwiftQrScanPlugin: NSObject, FlutterPlugin {
-  var flutterResult: FlutterResult? = nil
-  var captureSession: AVCaptureSession? = nil
+  private(set) var textureRegistry: FlutterTextureRegistry? = nil
+  
+  private(set) var flutterResult: FlutterResult? = nil
+  private(set) var captureSession: AVCaptureSession? = nil
   
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "io.cloudacy.qr_scan", binaryMessenger: registrar.messenger())
     let instance = SwiftQrScanPlugin()
+    instance.textureRegistry = registrar.textures()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    self.flutterResult = result
+    flutterResult = result
     
-    //result("iOS " + UIDevice.current.systemVersion)
-    // QR code test
-    /*
-    let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: nil)
-    let image = CIImage(contentsOf: URL(string: "https://support.apple.com/library/content/dam/edam/applecare/images/de_DE/iOS/ios12-iphone-x-camera-open-safari-qr-code.jpg")!)
-    let features = detector?.features(in: image!)
-    features?.forEach(
-      {(feature) -> Void in
-        if let qrFeature = feature as? CIQRCodeFeature {
-          result(qrFeature.messageString)
-        }
-      }
-    )
-    //result("count: \(features?.count)")
+    switch call.method {
+    case "init":
+      initializeQrScanner()
+      break
+    case "start":
+      startQrScanner()
+      break
+    default:
+      result(FlutterError(code: "InvalidMethod", message: "Invalid method \(call.method)!", details: nil))
+      break
+    }
+  }
+  
+  public func initializeQrScanner() {
+    guard let flutterResult = flutterResult else {
+      print("ERROR: No flutterResult available!")
+      return
+    }
     
-    */
+    let texture = SwiftQrScanPluginTexture()
+    let _textureId = textureRegistry?.register(texture)
+    
+    guard let textureId = _textureId else {
+      flutterResult(FlutterError(code: "NoTextureId", message: "Unable to fetch the textureId!", details: nil))
+      return
+    }
+    
     // Camera test
     self.captureSession = AVCaptureSession()
     // Begin/Commit configuration allows atomic configuration changes
     guard let captureSession = self.captureSession else {
-      result(FlutterError(code: "NoCaptureSession", message: "Unable to create a capture session!", details: nil))
+      flutterResult(FlutterError(code: "NoCaptureSession", message: "Unable to create a capture session!", details: nil))
       return
     }
     captureSession.beginConfiguration()
     
     // Try to find a camera device.
     guard let cameraDevice = findCameraDevice() else {
-      result(FlutterError(code: "NoCameraFound", message: "No camera device found!", details: nil))
+      flutterResult(FlutterError(code: "NoCameraFound", message: "No camera device found!", details: nil))
       return
     }
     cameraDevice.activeVideoMinFrameDuration = CMTime(seconds: 1.0, preferredTimescale: 1)
     
     // Try to set the found camera device as an input.
     guard let cameraDeviceInput = try? AVCaptureDeviceInput(device: cameraDevice), captureSession.canAddInput(cameraDeviceInput) else {
-      result(FlutterError(code: "NoCameraInput", message: "Unable to use the camera device as an input!", details: nil))
+      flutterResult(FlutterError(code: "NoCameraInput", message: "Unable to use the camera device as an input!", details: nil))
       return
     }
     
@@ -64,12 +93,32 @@ public class SwiftQrScanPlugin: NSObject, FlutterPlugin {
     
     captureSession.commitConfiguration()
     
+    texture.onFrameAvailable = {() -> Void in
+      self.textureRegistry?.textureFrameAvailable(textureId)
+    }
+    
+    flutterResult([
+      "textureId": textureId
+    ])
+  }
+  
+  public func startQrScanner() {
+    guard let flutterResult = self.flutterResult else {
+      print("ERROR: No flutterResult available!")
+      return
+    }
+    
+    guard let captureSession = captureSession else {
+      flutterResult(FlutterError(code: "NoCaptureSession", message: "Unable to create a capture session!", details: nil))
+      return
+    }
+    
     // Start scanning for a QR code.
     captureSession.startRunning()
   }
 
   public func findCameraDevice() -> AVCaptureDevice? {
-    let cameraDevice : AVCaptureDevice?
+    var cameraDevice: AVCaptureDevice?
     if #available(iOS 10.0, *) {
       cameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
     } else {
@@ -86,8 +135,12 @@ public class SwiftQrScanPlugin: NSObject, FlutterPlugin {
 extension SwiftQrScanPlugin: AVCaptureMetadataOutputObjectsDelegate {
   public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
     if let metadataObject = metadataObjects.first {
-      guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
-      guard let stringValue = readableObject.stringValue else { return }
+      guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else {
+        return
+      }
+      guard let stringValue = readableObject.stringValue else {
+        return
+      }
       // Stop scanning for more QR codes.
       if let captureSession = self.captureSession {
         captureSession.stopRunning()
--- lib/qr_scan.dart
+++ lib/qr_scan.dart
@@ -1,12 +1,38 @@
-import 'dart:async';
-
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
-class QrScan {
+class QrScan extends StatefulWidget {
+  @override
+  _QrScanState createState() => _QrScanState();
+}
+
+class _QrScanState extends State<QrScan> {
   static const MethodChannel _channel = const MethodChannel('io.cloudacy.qr_scan');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  @override
+  void initState() {
+    super.initState();
+    prepareCamera();
+  }
+
+  void prepareCamera() async {
+    final dynamic data = await _channel.invokeMapMethod('init');
+    print(data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        //Texture(textureId: null),
+        RaisedButton(
+          child: Text('start'),
+          onPressed: () {
+            _channel.invokeMethod('start');
+          },
+        ),
+      ],
+    );
   }
 }
