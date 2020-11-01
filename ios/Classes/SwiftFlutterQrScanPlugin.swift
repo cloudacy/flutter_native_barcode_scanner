0c230a5e9078f650220ea2886a7d6c8a60fbc371
Patrick Kuen <patrick.kuen11@gmail.com>
2020-11-01 12:24:32 +0100

    Improve iOS code.

--- ios/Classes/SwiftFlutterQrScanPlugin.swift
+++ ios/Classes/SwiftFlutterQrScanPlugin.swift
@@ -4,43 +4,35 @@ import AVFoundation
 import CoreMotion
 import libkern
 
-// Format used for video and image streaming.
-let videoFormat = FourCharCode(kCVPixelFormatType_32BGRA)
-
-protocol FrameExtractorDelegate: class {
-    func captured(image: UIImage)
-}
-
 @available(iOS 10.0, *)
 public class QrCam: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureMetadataOutputObjectsDelegate, FlutterTexture {
+  public let captureSession = AVCaptureSession()
   
-  private let position = AVCaptureDevice.Position.back
   private let quality = AVCaptureSession.Preset.medium
-  
-  private var permissionGranted = false
-  private let sessionQueue = DispatchQueue(label: "session queue")
-  private let captureSession = AVCaptureSession()
-  private let context = CIContext()
-  
   private var pixelBuffer: CVPixelBuffer?
-  
-  weak var delegate: FrameExtractorDelegate?
   
   private(set) var previewSize = CGSize(width: 1920, height: 1080)
   var onFrameAvailable: (() -> Void)?
   var methodChannel: FlutterMethodChannel?
   
+  private var videoOutput = AVCaptureVideoDataOutput()
+  private var metadataOutput = AVCaptureMetadataOutput()
   private var feedbackGenerator = UINotificationFeedbackGenerator()
+  
+  private var queue = DispatchQueue(label: "io.cloudacy.flutter_qr_scan")
   
   public init(methodChannel: FlutterMethodChannel) {
     super.init()
     
+    self.methodChannel = methodChannel
+  }
+  
+  public func startScanning() {
     // request access
     AVCaptureDevice.requestAccess(for: .video) { (granted) in
       if (granted) {
         self.configureSession()
         self.captureSession.startRunning()
-        self.methodChannel = methodChannel
       } else {
         // TODO: send error when no access to the camera was granted
       }
@@ -50,20 +42,14 @@ public class QrCam: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCa
   private func configureSession() {
     captureSession.beginConfiguration()
     captureSession.sessionPreset = quality
-    let videoDevice = AVCaptureDevice.default(for: .video)
+    guard let videoDevice = AVCaptureDevice.default(for: .video) else {return}
     guard
-      let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
+      let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice),
       captureSession.canAddInput(videoDeviceInput)
     else { return }
     captureSession.addInput(videoDeviceInput)
-    
-    let videoOutput = AVCaptureVideoDataOutput()
-    let metadataOutput = AVCaptureMetadataOutput()
-    
-    videoOutput.videoSettings = [
-      kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: videoFormat)
-    ]
-    videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sample buffer"))
+  
+    videoOutput.setSampleBufferDelegate(self, queue: queue)
     guard captureSession.canAddOutput(videoOutput) else { return }
     captureSession.addOutput(videoOutput)
     captureSession.addOutput(metadataOutput)
@@ -71,7 +57,7 @@ public class QrCam: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCa
     // configure metadataOutput
     
     // support qr codes
-    metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue(label: "sample buffer"))
+    metadataOutput.setMetadataObjectsDelegate(self, queue: queue)
     metadataOutput.metadataObjectTypes = [.qr, .ean8, .ean13, .code39, .code93, .code128, .pdf417, .upce, .dataMatrix]
     captureSession.commitConfiguration()
     
@@ -89,8 +75,6 @@ public class QrCam: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCa
   }
 
   public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-    captureSession.stopRunning()
-    
     if let metadataObject = metadataObjects.first {
       guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
       guard let stringValue = readableObject.stringValue else { return }
@@ -102,8 +86,7 @@ public class QrCam: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCa
         self.feedbackGenerator.notificationOccurred(.success)
       }
       
-      print("qr code found: " + stringValue)
-      methodChannel?.invokeMethod("barcode", arguments: stringValue)
+      methodChannel?.invokeMethod("code", arguments: stringValue)
     }
   }
 
@@ -118,21 +101,17 @@ public class QrCam: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCa
 
 @available(iOS 10.0, *)
 public class SwiftFlutterQrScanPlugin: NSObject, FlutterPlugin {
-  private var dispatchQueue: DispatchQueue?
-  
   private(set) var registry: FlutterTextureRegistry
   private(set) var messenger: FlutterBinaryMessenger
   private(set) var methodChannel: FlutterMethodChannel
   
-  private(set) var textureRegistry: FlutterTextureRegistry? = nil
-  
-  private(set) var flutterResult: FlutterResult? = nil
-  private(set) var captureSession: AVCaptureSession? = nil
+  private(set) var cam: QrCam
 
   init(registry: FlutterTextureRegistry, messenger: FlutterBinaryMessenger, methodChannel: FlutterMethodChannel) {
     self.registry = registry
     self.messenger = messenger
     self.methodChannel = methodChannel
+    self.cam = QrCam(methodChannel: methodChannel)
   }
   
   public class func register(with registrar: FlutterPluginRegistrar) {
@@ -143,16 +122,13 @@ public class SwiftFlutterQrScanPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if dispatchQueue == nil {
-      dispatchQueue = DispatchQueue(label: "io.cloudacy.flutter_qr_scan.dispatchQueue")
-    }
-    
     switch call.method {
     case "start":
       initializeQrScanner(call: call, result: result)
       break
     case "stop":
-      captureSession?.stopRunning()
+      cam.captureSession.stopRunning()
+      result(true)
       break
     default:
       result(FlutterError(code: "InvalidMethod", message: "Invalid method \(call.method)!", details: nil))
@@ -161,7 +137,7 @@ public class SwiftFlutterQrScanPlugin: NSObject, FlutterPlugin {
   }
 
   public func initializeQrScanner(call: FlutterMethodCall, result: FlutterResult) -> Void {
-    let cam = QrCam(methodChannel: methodChannel)
+    cam.startScanning()
     
     let textureId = registry.register(cam)
     
@@ -177,30 +153,6 @@ public class SwiftFlutterQrScanPlugin: NSObject, FlutterPlugin {
     
     result(resultObject)
   }
-  }
-
-
-@available(iOS 10.0, *)
-extension SwiftFlutterQrScanPlugin: AVCaptureMetadataOutputObjectsDelegate {
-    public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        if let metadataObject = metadataObjects.first {
-            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else {
-                return
-            }
-            guard let stringValue = readableObject.stringValue else {
-                return
-            }
-            // Stop scanning for more QR codes.
-            if let captureSession = self.captureSession {
-                captureSession.stopRunning()
-            }
-            // Send the result back.
-            print(stringValue)
-            if let result = self.flutterResult {
-                result(stringValue)
-            }
-        }
-    }
 }
 
 private func getFlutterError(error: Error?) -> FlutterError? {
